4803782987eee24f838c15943a1446e8b27710f8
Maeva Zulmea <maeva.zulmea@gmail.com>
2022-03-21 12:25:10 +0100

    nous contacter

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -10,37 +10,37 @@ import carteLoc from '../../image/carte.png'
 function ContactPage() {
 
     return(
-        <div className="row g-0 justify-content-center align-items-center"
-             style={{marginTop: "5em"}} >
-            <ABlock styles={{border: "none", width: "50%" }} >
-                <ABlock styles={{border: "1px solid black",
-                    display: "flex", justifyContent: "space-evenly"
-                }} >
-                    <ABlock styles={{border : "none", display: "flex",
-                        flexDirection: "column", alignItems: "center"}}>
-                        <AForm title="Connexion">
-                            <AInput label="Mom Prénom" type="text"/>
+        <div className="row g-0 justify-content-center align-items-center" style={{marginTop: "5em"}}>
+            <ABlock styles={{border: "none", width: "80%" }}>
+                <ABlock styles={{marginBottom:"4rem", border: "1px solid black", display: "flex"}}>
+                    <ABlock styles={{width:"50%", border : "none", display: "flex", flexDirection: "column", alignItems: "center"}}>
+                        <AForm title="Nous contacter">
+                            <AInput label="Nom prénom" type="text"/>
                             <AInput label="Adresse mail" type="text"/>
-                            <AInput label="Message" type="textarea"/>
+                            <AInput label="Object" type="textarea"/>
                             <div className="d-flex flex-column">
-                                <label className="col-form-label mt-4 text-start">Message</label>
+                                <label className="col-form-label mt-4 text-start">Votre message</label>
                                 <textarea className="form-control" ></textarea>
                             </div>
-                            <AButton styles={{border: "1px solid #707070", borderRadius: ".5em",
+                            <div className={"d-flex  justify-content-center mb-3"}>
+                                <AButton styles={{border: "1px solid #707070", borderRadius: ".5em",
                                 outline: "none",
                                 background: "inherit",
-                                marginTop: "1em"}}>
+                                marginTop: "1em",
+                                width: "100%"}}>
                                 Envoyer
-                            </AButton>
+                                </AButton>
+                            </div>
+
                         </AForm>
                     </ABlock>
-                    <ABarreVertical styles={{height: "29rem"}}/>
+                    <ABarreVertical styles={{height: "29rem", marginRight: "7rem"}}/>
                     <ABlock styles={{border : "none",
                         display: "flex",
                         flexDirection: "column",
                         alignItems: "center",
                         width: "30%"}}>
-                        <h2>Nous Trouvez</h2>
+                        <h2>Nous Trouver</h2>
                         <img src={carteLoc} style={{maxWidth: "100%",
                             height: 'auto'}}/>
                         <a href="https://goo.gl/maps/i9vSAsCq1ywSsdcC9">Ouvrir Carte</a>
@@ -49,7 +49,6 @@ function ContactPage() {
                             Grand Gouffre de la principauté de Shéogorath
                             Oblivion
                         </ABlock>
-
                     </ABlock>
                 </ABlock>
             </ABlock>
